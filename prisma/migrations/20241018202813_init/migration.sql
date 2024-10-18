782d723eb1b7694fa358dce860ee6fddac21f605
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-18 22:30:20 +0200

    Create Session model

---
+++ prisma/migrations/20241018202813_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sid" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sid_key" ON "Session"("sid");
